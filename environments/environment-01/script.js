fb5ad19879186bbe815c7bb20b3d79956de5d59e
AliHMohammad <alihmohammad@hotmail.com>
2023-04-26 15:46:23 +0200

    Øvelser i cafe digital

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,54 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+async function initApp() {
+    const dataArray = await getData();
+    loopUsers(dataArray)
+    
+    const adminArray = filterArray(dataArray)
+    loopUsers(adminArray)
+
+
+    
+}
+
+async function getData() {
+    const response = await fetch("users.json");
+    const data = await response.json();
+
+    return data
+}
+
+function loopUsers(users) {
+
+    for (const user of users) {
+        showUser(user)
+    }
+    const html = /*html*/`
+    <h2>=========</h2>
+    `
+
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
+
+function showUser(objekt) {
+    const html = /*html*/`
+    <li>Name: ${objekt.name} - Status: ${objekt.active}</li>
+    `
+
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
+
+function filterArray(dataArray) {
+    const resultArray = dataArray.filter(filterAdmins)
+
+    function filterAdmins(objekt) {
+        if (objekt.role == "admin") {
+            return objekt
+        }
+    }
+
+    return resultArray
+}
+
--- exercises/exercise-01.md
+++ exercises/exercise-01.md
@@ -2,6 +2,6 @@
 
 Environment: `environment-01`
 
-1. Lav en funktion der indlæser JSON-filen `users.json` og gemmer listen i en variabel.
-2. Lav en funktion der viser listen på websiden - hver user skal vises med navn og hvorvidt de er aktive eller ej.
-3. Filtrér listen så den kun viser admin-brugere.
+1. Lav en funktion der indlæser JSON-filen `users.json` og gemmer listen i en variabel. (Fetch)
+2. Lav en funktion der viser listen på websiden - hver user skal vises med navn og hvorvidt de er aktive eller ej. (DOM-manipulation)
+3. Filtrér listen så den kun viser admin-brugere. (Filter / DOM)
