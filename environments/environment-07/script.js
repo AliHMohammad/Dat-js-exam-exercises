4b9f211dc0a886471a0e5b2c2df0675f0c8d656d
AliHMohammad <alihmohammad@hotmail.com>
2023-05-30 11:00:40 +0200

    Øvelse i klassen

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+
+window.addEventListener("load", start);
+
+let students = [];
+
+function start(event) {
+    console.log("script is running!");
+    document.querySelector("#create-student-form").addEventListener("submit", submitForm);
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const name = form.name.value;
+    const email = form.email.value;
+    const age = Number(form.age.value);
+
+    createStudent(name, email, age);
+}
+
+function createStudent(name, email, age) {
+    const newStudent = { name, email, age };
+    students.push(newStudent);
+    console.log(students);
+    checkCorrectEmail(newStudent);
+}
+
+function checkCorrectEmail(student) {
+
+    
+    if (!student.email.endsWith("@stud.kea.dk" || !student.email.length > 3)) {
+        console.log("This user does not meet requirements. User removed");
+        students.pop();
+    }
+    console.log(students);
+}
