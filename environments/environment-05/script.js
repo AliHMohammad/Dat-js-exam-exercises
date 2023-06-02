23a1a40f766790f00f7249e1c14fe58e5ef55fda
AliHMohammad <alihmohammad@hotmail.com>
2023-06-02 23:32:25 +0200

    Øvelse 13 done, 10min

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,38 @@
 "use strict";
+
+import { courses } from "./courses.js";
+
+window.addEventListener("load", start);
+
+function start(event) {
+    console.log("script is running");
+    document.querySelector("#select-filter-ects").addEventListener("change", filterAndDisplayCourses);
+    filterAndDisplayCourses()
+}
+
+function showCourses(courses) {
+    document.querySelector("#courses-list").innerHTML = "";
+    
+    for (const course of courses) {
+        showCourse(course)
+    }
+}
+
+function showCourse(course) {
+    
+    const html = /*html*/ `
+    <li>${course.name}, ECTS: ${course.ectsPoints}</li>
+    `;
+
+    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+}
+
+function filterAndDisplayCourses(event) {
+    const filteredCourses = filter()
+    showCourses(filteredCourses);
+}
+
+function filter() {
+    const filterValue = document.querySelector("#select-filter-ects").value;
+    return courses.filter((course) => course.ectsPoints === Number(filterValue));
+}
