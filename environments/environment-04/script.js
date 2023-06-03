f091efd79e0a7f0db19eddf60bbb50c7106f9a3c
AliHMohammad <alihmohammad@hotmail.com>
2023-06-03 13:40:22 +0200

    Øvelse 10 done. 8min

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", start);
+
+function start(event) {
+    console.log(teachers);
+    showTeachers(teachers);
+    addNewTeacher("Ali", "almo@stud.kea.dk")
+}
+
+function showTeachers(teachers) {
+    document.querySelector("#teachers-list").innerHTML = "";
+    
+    for (let i = 0; i < teachers.length; i++){
+        showTeacher(teachers[i]);
+    }
+}
+
+function showTeacher(teacher) {
+    const html = /*html*/ `
+    <li>${teacher.name} - E-mail: ${teacher.email}</li>
+    `;
+
+    document.querySelector("#teachers-list").insertAdjacentHTML("beforeend", html);
+}
+
+function addNewTeacher(name, email) {
+    const newTeacher = {name, email};
+
+    teachers.push(newTeacher);
+    showTeachers(teachers);
+}
