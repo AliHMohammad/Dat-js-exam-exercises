3123f88c816c0d3d32d478174c6c97a54be73143
AliHMohammad <alihmohammad@hotmail.com>
2023-06-03 13:55:45 +0200

    Øvelse 12 done. 5 min

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+import { courses } from "./courses.js";
+
+window.addEventListener("load", start);
+
+function start(event) {
+    console.log(courses);
+    sortCoursesAfterECTS();
+    showCourses(courses);
+}
+
+function showCourses(courses) {
+    for (let i = 0; i < courses.length; i++) {
+        showCourse(courses[i]);
+    }
+}
+
+function showCourse(course) {
+    const html = /*html*/ `
+    <li> ${course.name} - ${course.ectsPoints} ECTS points <button>Get Course Name</button></li>
+    
+    `;
+
+    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+}
+
+function sortCoursesAfterECTS() {
+    courses.sort((a, b) => b.ectsPoints - a.ectsPoints);
+}
+
