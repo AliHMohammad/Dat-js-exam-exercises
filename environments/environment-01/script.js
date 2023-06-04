cacfcdc68ca6124ec0f43d261a8c68d4b73e2a06
AliHMohammad <alihmohammad@hotmail.com>
2023-06-04 13:43:03 +0200

    Øvelse 2 done. 10min

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,42 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+let users = [];
+
+async function start(event) {
+    await getUsers();
+    console.log(users);
+    showUsers(users)
+    showRoleCount(users)
+}
+
+async function getUsers() {
+    const promise = await fetch("./users.json");
+    users = await promise.json();
+}
+
+function showUsers(users) {
+    
+    for (const user of users) {
+        showUser(user);
+    }
+}
+
+function showUser(user) {
+    const html = /*html*/`
+    <li>${user.name} - Role: ${user.role}</li>
+    `
+
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
+
+function getUserRolesCount(users, userRole) {
+    return users.filter(user => user.role === userRole).length;
+}
+
+function showRoleCount(users) {
+    document.querySelector("#admin-count").textContent = getUserRolesCount(users, "admin");
+    document.querySelector("#user-count").textContent = getUserRolesCount(users, "user");
+    document.querySelector("#guest-count").textContent = getUserRolesCount(users, "guest");
+}
