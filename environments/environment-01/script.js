b4c9e7bea162664d80c06b0d5ddb86e19bc3d785
AliHMohammad <alihmohammad@hotmail.com>
2023-06-06 09:37:12 +0200

    Done. 07:04

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,43 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+let users = [];
+
+async function start(event) {
+    await getUsers();
+    console.log(users);
+    // showUsers(users)
+    users.forEach(showUser);
+    showUserRoles();
+}
+
+async function getUsers() {
+    const response = await fetch("./users.json");
+    const data = await response.json();
+    users = data;
+}
+
+function showUsers(users) {
+    for (let i = 0; i < users.length; i++) {
+        showUser(users[i]);
+    }
+}
+
+function showUser(user) {
+    const html = /*html*/ `
+    <li>${user.name}, Role: ${user.role}</li>
+    `;
+
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
+
+function countUserRoles(role) {
+    return users.filter((user) => user.role === role).length;
+}
+
+function showUserRoles() {
+    document.querySelector("#admin-count").textContent = countUserRoles("admin");
+    document.querySelector("#user-count").textContent = countUserRoles("user");
+    document.querySelector("#guest-count").textContent = countUserRoles("guest");
+}
