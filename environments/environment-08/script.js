af5c387d40738fa79116690b1da3a484cd46e945
AliHMohammad <alihmohammad@hotmail.com>
2023-06-06 12:30:19 +0200

    Øvelse 23 done. 10:18

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,63 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let playlist = [
+    {
+        artist: "Travis Scott",
+        title: "90210",
+        duration: "3:20",
+    },
+    {
+        artist: "Rihanna",
+        title: "Work",
+        duration: "2:20",
+    },
+    {
+        artist: "Drake",
+        title: "One Dance",
+        duration: "4:20",
+    },
+];
+
+function start(event) {
+    console.log("script");
+    playlist = preparePlaylist();
+    showPlaylist(playlist);
+}
+
+function preparePlaylist() {
+    const preparedPlaylist = [];
+
+    for (const song of playlist) {
+        const newSong = { song, upvotes: 0 };
+        preparedPlaylist.push(newSong);
+    }
+
+    return preparedPlaylist;
+}
+
+function showPlaylist(playlist) {
+    document.querySelector("#songlist").innerHTML = "";
+    playlist.sort((a, b) => b.upvotes - a.upvotes);
+
+    for (let i = 0; i < playlist.length; i++) {
+        showSong(playlist[i]);
+    }
+}
+
+function showSong(object) {
+    const html = /*html*/ `
+    <li><button>Upvote</button> ${object.song.artist}: ${object.song.title}. UPVOTES: ${object.upvotes}</li>
+    `;
+
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+
+    document.querySelector("#songlist li:last-child button").addEventListener("click", () => upvoteSong(object));
+}
+
+function upvoteSong(song) {
+    console.log(song.song.title);
+    song.upvotes += 1;
+    showPlaylist(playlist);
+}
