8647aab4cf78c0e0ecc240b7ddee4202ef2c62a8
AliHMohammad <alihmohammad@hotmail.com>
2023-06-06 11:04:48 +0200

    done: 06:06min

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let playlist = [];
+
+async function start(event) {
+    console.log("script is running!");
+    playlist = await getPlaylist();
+    console.log(playlist);
+    showPlaylist(playlist);
+}
+
+async function getPlaylist() {
+    const response = await fetch("./playlist.json");
+    return await response.json();
+}
+
+function showPlaylist(playlist) {
+    document.querySelector("#songlist").innerHTML = "";
+
+    let i = 0;
+    while (i < playlist.length) {
+        const html = /*html*/ `
+        <li> <button>Remove</button> ${playlist[i].artist}: ${playlist[i].title}</li>
+        
+        `;
+
+        document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+
+        document.querySelector("#songlist li:last-child button").addEventListener("click", () => removeSong(playlist[i]));
+        i++;
+    }
+}
+
+function removeSong(song) {
+    const index = playlist.indexOf(song);
+    playlist.splice(index, 1);
+    showPlaylist(playlist);
+}
